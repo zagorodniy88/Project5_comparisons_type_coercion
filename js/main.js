0cb10c43495c7d0ac76b27ddc5fedab18257d60c
Alex <zagorodniy88@gmail.com>
2021-09-28 21:49:49 -0300

    Project5_comparisonsn

---
+++ js/main.js
@@ -0,0 +1,14 @@
+let num = 100, text = 'string', bool = true;
+
+document.write(typeof num + '<br>');
+document.write(typeof text + '<br>');
+document.write(typeof bool + '<br>');
+
+let a = '10' + 5;
+document.write(a + '<br>');
+
+if(text == String || num >= 100 ){
+    document.write('JS the best');
+}else{
+    document.write('Something went wrong');
+}
